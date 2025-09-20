0cf4d033363a439c152a0c93102fb841a75ab281
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-09-20 06:03:32 -0400

    Update Site

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   "fullName": "Alex",
   "email": "alex@gmail.com",
   "giveawayName": "Arslan",
-  "submittedAt": "2025-09-20T09:59:24.447Z",
+  "submittedAt": "2025-09-20T10:03:30.693Z",
   "deploymentInfo": {
     "repoName": "Giveaway-586",
     "repoUrl": "https://github.com/arslvn93/Giveaway-586",
